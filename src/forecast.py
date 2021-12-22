88a310244cdb977ba607fc91aa319e03bc68b7ff
VenMolom <venmolom@protonmail.com>
2021-12-22 15:18:25 +0100

    docs almost ready

--- src/forecast.py
+++ src/forecast.py
@@ -23,31 +23,37 @@ def build_model(data_frame: DataFrame, auto: bool):
 
     if auto:
         print('[Finding parameters]')
-        # ARIMA(2,0,2)(1,0,1)[12] intercept   : AIC=2907.243, Time=13.19 sec
-        # ARIMA(0,0,0)(0,0,0)[12] intercept   : AIC=4233.554, Time=0.06 sec
-        # ARIMA(1,0,0)(1,0,0)[12] intercept   : AIC=3240.299, Time=4.07 sec
-        # ARIMA(0,0,1)(0,0,1)[12] intercept   : AIC=3550.434, Time=1.99 sec
-        # ARIMA(0,0,0)(0,0,0)[12]             : AIC=6323.297, Time=0.09 sec
-        # ARIMA(2,0,2)(0,0,1)[12] intercept   : AIC=2905.260, Time=10.23 sec
-        # ARIMA(2,0,2)(0,0,0)[12] intercept   : AIC=2918.394, Time=1.25 sec
-        # ARIMA(2,0,2)(0,0,2)[12] intercept   : AIC=2933.561, Time=24.18 sec
-        # ARIMA(2,0,2)(1,0,0)[12] intercept   : AIC=inf, Time=9.48 sec
-        # ARIMA(2,0,2)(1,0,2)[12] intercept   : AIC=inf, Time=35.66 sec
-        # ARIMA(1,0,2)(0,0,1)[12] intercept   : AIC=3404.582, Time=2.99 sec
-        # ARIMA(2,0,1)(0,0,1)[12] intercept   : AIC=3219.072, Time=8.15 sec
-        # ARIMA(3,0,2)(0,0,1)[12] intercept   : AIC=inf, Time=10.32 sec
-        # ARIMA(2,0,3)(0,0,1)[12] intercept   : AIC=2872.378, Time=9.52 sec
-        # ARIMA(2,0,3)(0,0,0)[12] intercept   : AIC=inf, Time=1.82 sec
-        # ARIMA(2,0,3)(1,0,1)[12] intercept   : AIC=inf, Time=12.12 sec
-        # ARIMA(2,0,3)(0,0,2)[12] intercept   : AIC=2874.057, Time=24.74 sec
-        # ARIMA(2,0,3)(1,0,0)[12] intercept   : AIC=inf, Time=10.39 sec
-        # ARIMA(2,0,3)(1,0,2)[12] intercept   : AIC=inf, Time=28.29 sec
-        # ARIMA(1,0,3)(0,0,1)[12] intercept   : AIC=3379.934, Time=4.15 sec
-        # ARIMA(3,0,3)(0,0,1)[12] intercept   : AIC=2905.880, Time=10.44 sec
-        # ARIMA(2,0,4)(0,0,1)[12] intercept   : AIC=inf, Time=12.89 sec
-        # ARIMA(1,0,4)(0,0,1)[12] intercept   : AIC=3356.046, Time=10.71 sec
-        # ARIMA(3,0,4)(0,0,1)[12] intercept   : AIC=2892.519, Time=13.98 sec
-        # ARIMA(2,0,3)(0,0,1)[12]             : AIC=3538.128, Time=4.17 sec
+        # ARIMA(2,0,2)(1,0,1)[12] intercept   : AIC=inf, Time=17.35 sec
+        # ARIMA(0,0,0)(0,0,0)[12] intercept   : AIC=4171.137, Time=0.07 sec
+        # ARIMA(1,0,0)(1,0,0)[12] intercept   : AIC=2771.028, Time=4.13 sec
+        # ARIMA(0,0,1)(0,0,1)[12] intercept   : AIC=3275.026, Time=1.75 sec
+        # ARIMA(0,0,0)(0,0,0)[12]             : AIC=5671.382, Time=0.03 sec
+        # ARIMA(1,0,0)(0,0,0)[12] intercept   : AIC=3381.856, Time=0.22 sec
+        # ARIMA(1,0,0)(2,0,0)[12] intercept   : AIC=inf, Time=13.20 sec
+        # ARIMA(1,0,0)(1,0,1)[12] intercept   : AIC=inf, Time=8.24 sec
+        # ARIMA(1,0,0)(0,0,1)[12] intercept   : AIC=3133.417, Time=1.50 sec
+        # ARIMA(1,0,0)(2,0,1)[12] intercept   : AIC=inf, Time=22.26 sec
+        # ARIMA(0,0,0)(1,0,0)[12] intercept   : AIC=inf, Time=3.42 sec
+        # ARIMA(2,0,0)(1,0,0)[12] intercept   : AIC=2769.029, Time=6.94 sec
+        # ARIMA(2,0,0)(0,0,0)[12] intercept   : AIC=3110.499, Time=0.37 sec
+        # ARIMA(2,0,0)(2,0,0)[12] intercept   : AIC=inf, Time=19.03 sec
+        # ARIMA(2,0,0)(1,0,1)[12] intercept   : AIC=inf, Time=10.40 sec
+        # ARIMA(2,0,0)(0,0,1)[12] intercept   : AIC=3062.754, Time=2.24 sec
+        # ARIMA(2,0,0)(2,0,1)[12] intercept   : AIC=inf, Time=25.03 sec
+        # ARIMA(3,0,0)(1,0,0)[12] intercept   : AIC=2768.910, Time=9.63 sec
+        # ARIMA(3,0,0)(0,0,0)[12] intercept   : AIC=2862.989, Time=0.37 sec
+        # ARIMA(3,0,0)(2,0,0)[12] intercept   : AIC=inf, Time=30.56 sec
+        # ARIMA(3,0,0)(1,0,1)[12] intercept   : AIC=2391.583, Time=13.28 sec
+        # ARIMA(3,0,0)(0,0,1)[12] intercept   : AIC=2857.519, Time=2.92 sec
+        # ARIMA(3,0,0)(2,0,1)[12] intercept   : AIC=inf, Time=34.97 sec
+        # ARIMA(3,0,0)(1,0,2)[12] intercept   : AIC=inf, Time=27.37 sec
+        # ARIMA(3,0,0)(0,0,2)[12] intercept   : AIC=2833.304, Time=15.06 sec
+        # ARIMA(3,0,0)(2,0,2)[12] intercept   : AIC=inf, Time=35.84 sec
+        # ARIMA(4,0,0)(1,0,1)[12] intercept   : AIC=2469.696, Time=14.71 sec
+        # ARIMA(3,0,1)(1,0,1)[12] intercept   : AIC=inf, Time=18.41 sec
+        # ARIMA(2,0,1)(1,0,1)[12] intercept   : AIC=inf, Time=13.63 sec
+        # ARIMA(4,0,1)(1,0,1)[12] intercept   : AIC=inf, Time=nan sec
+        # ARIMA(3,0,0)(1,0,1)[12]             : AIC=inf, Time=12.70 sec
         model = pm.auto_arima(data_frame,
                             p=1,
                             seasonal=True,
@@ -58,7 +64,7 @@ def build_model(data_frame: DataFrame, auto: bool):
                             stepwise=True)
     else:
         print('[Using manual parameters]')
-        model = m.ARIMA(data_frame,order=(2, 0, 3), seasonal_order=(0, 0, 2, 12)).fit()
+        model = m.ARIMA(data_frame,order=(3, 0, 0), seasonal_order=(1, 0, 1, 12)).fit()
 
     print('[Model builded]')
     return model
@@ -100,14 +106,13 @@ def analyze(data: DataFrame):
 def main():
     df = read_df()
 
-    analyze(df)
-    exit()
+    # analyze(df)
 
     split = 756 # 63 year of train data, 16 years of test data (around 80/20 split)
     train = df[:split]
     test = df[split:]
 
-    auto = False
+    auto = True
     smodel = build_model(train, auto)
     smodel.summary()
 
